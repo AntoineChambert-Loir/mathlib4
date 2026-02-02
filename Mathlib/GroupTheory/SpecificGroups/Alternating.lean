33e27d046637bbaea2cd4456262be525962eeed3
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-02-02 12:26:18 +0100

    simplify proof

--- Mathlib/GroupTheory/SpecificGroups/Alternating.lean
+++ Mathlib/GroupTheory/SpecificGroups/Alternating.lean
@@ -253,11 +253,8 @@ theorem closure_isCycleType22_eq_top (h5 : 5 ≤ Nat.card α) :
     aesop
   ext g
   refine ⟨fun ⟨k, _⟩ ↦ by simp_all, fun hg ↦ ⟨⟨g, by
-    -- `exact hg.mem_alternatingGroup` used to work
     simp only [Set.mem_setOf_eq] at hg
-    suffices (-1 : ℤˣ) ^ 6 = 1 by
-      simp [this, mem_alternatingGroup, sign_of_cycleType, hg]
-    rw [Even.neg_pow ⟨3, by norm_num⟩, one_pow]⟩, by simpa⟩⟩
+    simp [sign_of_cycleType, hg, ← Units.val_inj]⟩, by simpa⟩⟩
 
 /-- A key lemma to prove $A_5$ is simple. Shows that any normal subgroup of an alternating group on
   at least 5 elements is the entire alternating group if it contains a 3-cycle. -/
