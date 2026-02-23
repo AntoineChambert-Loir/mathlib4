c22b5e287633542b61fe5933931b9f30713d600b
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-02-23 19:58:54 +0100

    feat(Algebra/TrivSqZeroExt): the ideal has square zero

--- Mathlib/Algebra/TrivSqZeroExt.lean
+++ Mathlib/Algebra/TrivSqZeroExt.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2020 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kenny Lau, Eric Wieser
+Authors: Kenny Lau, Eric Wieser, Antoine Chambert-Loir, María-Inés de Frutos-Fernández
 -/
 module
 
@@ -10,6 +10,7 @@ public import Mathlib.Algebra.GroupWithZero.Invertible
 public import Mathlib.LinearAlgebra.Prod
 public import Mathlib.Algebra.Algebra.Subalgebra.Lattice
 public import Mathlib.Algebra.Order.Group.Nat
+public import Mathlib.RingTheory.Ideal.Maps
 
 /-!
 # Trivial Square-Zero Extension
@@ -1132,6 +1133,33 @@ theorem map_comp_map (f : M →ₗ[R'] N) (g : N →ₗ[R'] P) :
   simp only [map_inr, LinearMap.coe_comp, Function.comp_apply, AlgHom.coe_comp, forall_const]
 
 end map
+
+section Ideal
+
+open Ideal
+
+variable (R M : Type*)
+  [CommSemiring R] [AddCommMonoid M] [Module R M] [Module Rᵐᵒᵖ M] [IsCentralScalar R M]
+
+/-- The kernel of the `AlgHom` `fstHom R R M` -/
+def kerIdeal : Ideal (TrivSqZeroExt R M) := RingHom.ker (fstHom R R M)
+
+theorem mem_kerIdeal_iff_inr (x : TrivSqZeroExt R M) : x ∈ kerIdeal R M ↔ x = inr x.snd := by
+  obtain ⟨r, m⟩ := x
+  simp only [kerIdeal, RingHom.mem_ker, fstHom_apply, fst_mk]
+  exact ⟨fun hr => by rw [hr]; rfl, fun hrm => by rw [← fst_mk r m, hrm, fst_inr]⟩
+
+theorem mem_kerIdeal_iff_exists (x : TrivSqZeroExt R M) :
+    x ∈ kerIdeal R M ↔ ∃ m : M, x = inr m := by
+  rw [mem_kerIdeal_iff_inr]
+  exact ⟨fun h => ⟨x.snd, h⟩, fun ⟨m, hm⟩ => by rw [hm]; rfl⟩
+
+theorem sqZero : kerIdeal R M ^ 2 = (⊥ : Ideal (TrivSqZeroExt R M)) := by
+  simp only [pow_two, eq_bot_iff, mul_le, mem_kerIdeal_iff_inr]
+  rintro x hx y hy
+  rw [hx, hy, mem_bot, inr_mul_inr]
+
+end Ideal
 
 end Algebra
 
