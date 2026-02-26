d9268133747c9e3e735a9b0cecb075071132517e
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-02-27 00:44:18 +0100

    correct copyright header

--- Mathlib/RingTheory/GradedAlgebra/Homogeneous/Hom.lean
+++ Mathlib/RingTheory/GradedAlgebra/Homogeneous/Hom.lean
@@ -1,4 +1,5 @@
-/- Copyright (c) 2021 Antoine Chambert-Loir, María-Inés de Frutos-Fernández. All rights reserved.
+/-
+Copyright (c) 2026 Antoine Chambert-Loir, María-Inés de Frutos-Fernández. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir, María-Inés de Frutos-Fernández
 -/
